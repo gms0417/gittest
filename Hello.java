ebbee618fb915ea5dec7080ed9f8d440ac0813a1
gms0417 <61400131+gms0417@users.noreply.github.com>
2020-03-10 11:34:43 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ class Hello{
 	public static void main(String[] args){
 		system.out.println("Hello");
 		system.out.println("World");
-		system.out.println("�ȳ��ϼ���");
+		system.out.println("안녕하세요");
+		system.out.println("안녕하세요");
 	}
 
 }
